ccb2cd3cdc8e3afd3ae56852cb2be7e9cf75af00
Yanuar Aditia <yanuaraditia@outlook.com>
2026-04-14 11:12:14 +0700

    feat: update release workflow and versioning to use dynamic GEM_VERSION from tags

--- lib/kiriminaja.rb
+++ lib/kiriminaja.rb
@@ -3,5 +3,8 @@
 require_relative "kiriminaja/client"
 
 module KiriminAja
-  VERSION = "0.1.0"
+  VERSION = ENV.fetch("GEM_VERSION") {
+    tag = `git describe --tags --abbrev=0 2>/dev/null`.strip
+    tag.start_with?("v") ? tag[1..] : "0.0.0"
+  }
 end
